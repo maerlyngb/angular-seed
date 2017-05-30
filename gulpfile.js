f7758be2df5d3ba1455b5d8cd26059fb72b5067b
Maerlyn Broadbent <maerlyn.broadbent@ge.com>
2017-05-30 12:20:24 +1000

    gulp connect

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,15 @@ function getTask(task) {
 	return require('./tasks/' + task)(gulp, APP_DIR);
 }
 
+// compile sass
 gulp.task('sass', getTask('sass'));
 
+// run app
+gulp.task('serve', getTask('serve'));
+
+// watch for file changes and do any required processing
 gulp.task('watch', function() {
   gulp.watch(APP_DIR + 'styles/sass/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'serve', 'watch']);
